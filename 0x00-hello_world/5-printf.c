b9f38a4499980090f11971598ea6573554d92e94
root <jessicaogechi4@gmail.com>
2023-02-22 05:18:23 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - print "with proper grammar, but the outcome is a piece of art," followed new line
+ * main - print "with proper grammar, but the outcome is a piece of art," 
  * Return: Always 0.
  *
  */
