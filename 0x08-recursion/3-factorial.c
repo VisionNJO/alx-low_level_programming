e9a26b6884ce124f88579da6b0c242e310a5f39b
VisionNJO <jessicaogechi4@gmail.com>
2023-06-12 07:19:40 -0700

    3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,18 +6,15 @@
  * @n: number  of  factorial
  * Return: Always (Success)
  */
-int factorial(int n)
+int factorial(int n) 
 {
-	if (n < 0)
-	{
-		return (-1);
-	}
-	else if (n == 0)
-	{
-		return (1);
-	}
-	else
-	{
-		return (n * factorial(n - 1));
-	}
+    if (n == 0) 
+    {
+        return 1;
+    } 
+    else 
+    {
+        return n * factorial(n - 1);
+    }
 }
+
